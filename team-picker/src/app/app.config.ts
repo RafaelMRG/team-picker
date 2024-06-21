d91098524493023271189eb2ad81b1064ecf7206
Rafael <rafael.vozikis@hotmail.com>
2024-06-21 18:34:10 -0300

    config: Disabled tooltip interactivity globally

--- team-picker/src/app/app.config.ts
+++ team-picker/src/app/app.config.ts
@@ -3,11 +3,20 @@ import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MAT_TOOLTIP_DEFAULT_OPTIONS } from "@angular/material/tooltip";
 
 export const appConfig: ApplicationConfig = {
 	providers: [
-		provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideRouter(
 			routes,
-			withHashLocation()), provideAnimationsAsync()
+			withHashLocation()),
+		provideAnimationsAsync(),
+		{
+			provide:  MAT_TOOLTIP_DEFAULT_OPTIONS,
+			useValue: {
+				disableTooltipInteractivity: true,
+			},
+		},
 	]
 };
